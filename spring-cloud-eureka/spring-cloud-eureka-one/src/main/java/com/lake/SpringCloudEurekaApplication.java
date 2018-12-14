c884499c7565435eb77576c57c626551fea36c4a
hubin.f <hubin.f@sunyard.com>
2018-12-14 15:05:57 +0800

    单个Spring Cloud Eureka server

---
+++ spring-cloud-eureka/spring-cloud-eureka-one/src/main/java/com/lake/SpringCloudEurekaApplication.java
@@ -0,0 +1,19 @@
+package com.lake;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * <code>@EnableEurekaServer</code> 表示这是一个Eureka Server
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class SpringCloudEurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudEurekaApplication.class, args);
+	}
+
+}
+
